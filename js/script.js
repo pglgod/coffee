cf6bafbc051c77f0b8d9a4c6a0c3f6941a520beb
Amit Vishwakarma <111479783+pglgod@users.noreply.github.com>
2022-11-21 22:59:40 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+//var navi = documnet.getElementById("navbar");
+var searchbar = document.getElementById("searchbar");
+var searchicn = document.getElementById("search-icn");
+
+//function navcolor() {
+  //  navi.style.backgroundColor = "rgba(255, 255, 255, 0.847)";
+//};
+function show_searchbar() {
+    searchbar.style.display = "unset";
+    searchicn.style.display = "none";
+};
